ea4a85b001ba10a79f1a302cff0389eb07c9f70b
Harry Lee <leelee@fullsail.edu>
2012-05-02 08:08:10 -0400

    New files

---
+++ MIU/js/json.js
@@ -0,0 +1,36 @@
+var json = {
+	"book1": {
+	    "lists:": ["Lists:", "Books Read"],
+		"author": ["Author:", "Brandon Sanderson"],
+		"title": ["Title:", "Warbreaker" ],
+		"date":	 ["Date:", "2012-04-20"],
+		"genre": ["Genre", "Fiction"],
+		"age": ["Age:", "Adult"],
+		"subject": ["Subject:", "Science Fiction & Fantasy"],
+		"rate":	["Rate:", "8"],
+		"comments":	["Comments", "69788"]
+	},
+	"book2": {
+	    "lists:": 		["Lists:", "Books Read"],
+		"author": 		["Author:", "Jonathan Stroud"],
+		"title":  		["Title:", "The Ring of Solomon"],
+		"date":	  		["Date:", "2012-04-21"],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Science Fiction & Fantasy"],
+		"rate":			["Rate", "10"],
+		"comments":		["Comments", "72274"]
+	},
+	"book3": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Aliso Goodman"],
+		"title":  		["Title:", "Eona"],
+		"date":	  		["Date:",],
+		"genre":		["Genre", "Nonfiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Science Fiction & Fantasy"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73260"]
+	
+	}
+}
