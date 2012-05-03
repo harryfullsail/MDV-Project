ebd756c4c7a09e097dd14cd3bc9f8052847ac6f1
Harry Lee <leelee@fullsail.edu>
2012-05-03 18:39:51 -0400

    Added More dummy data in the json.js file.

--- MIU/js/json.js
+++ MIU/js/json.js
@@ -31,6 +31,211 @@ var json = {
 		"subject":		["Subject:", "Science Fiction & Fantasy"],
 		"rate":			["Rate", "1"],
 		"comments":		["Comments", "73260"]
+		
+			},
+	"book4": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Charlains Harris"],
+		"title":  		["Title:", "Deadlocked"],
+		"date":	  		["Date:",],
+		"genre":		["Genre", "fiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Science Fiction & Fantasy"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "Sookkie Stackhouse Book 12 southern Vampire series"]
+		
+		},
+	"book5": {
+	    "lists:": 		["Lists:", "Books Read"],
+		"author": 		["Author:", "Wise Brown"],
+		"title":  		["Title:", "Goodnight Moon"],
+		"date":	  		["Date:","2012-04-21"],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "0-2"],
+		"subject":		["Subject:", "Early Readers"],
+		"rate":			["Rate", "8"],
+		"comments":		["Comments", "73260"]
+		
+		},
+	"book6": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Brianna Caplan Sayres"],
+		"title":  		["Title:", "Where Do Diggers Sleep at Night"],
+		"date":	  		["Date:",""],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "0-2"],
+		"subject":		["Subject:", "Early Readers"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73260"]
+		
+		},
+	"book7": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Laura Numeroff"],
+		"title":  		["Title:", "If You Give A Mouse A Cookie"],
+		"date":	  		["Date:",""],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "0-2"],
+		"subject":		["Subject:", "Early Readers"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73260"]
+		
+		},
+	"book8": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Brianna Caplan Sayres"],
+		"title":  		["Title:", "Where Do Diggers Sleep at Night"],
+		"date":	  		["Date:",""],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "0-2"],
+		"subject":		["Subject:", "Early Readers"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73260"]
+		
+		},
+	"book9": {
+	    "lists:": 		["Lists:", "Books read"],
+		"author": 		["Author:", "Dr. Seuss"],
+		"title":  		["Title:", "Cat In The Hat"],
+		"date":	  		["Date:","2012-04-21"],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "0-2"],
+		"subject":		["Subject:", "Classics"],
+		"rate":			["Rate", "10"],
+		"comments":		["Comments", "73260"]
+		
+		},
+	"book10": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Melinda Lang"],
+		"title":  		["Title:", "How I Became A Pirate"],
+		"date":	  		["Date:","2012-04-21"],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "3-5"],
+		"subject":		["Subject:", "Early Readers"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73260"]
+		
+		},
+	"book11": {
+	    "lists:": 		["Lists:", "Books read"],
+		"author": 		["Author:", "Gaby Rodriguez"],
+		"title":  		["Title:", "The Pregnancy Project: A Memoir"],
+		"date":	  		["Date:","2012-04-21"],
+		"genre":		["Genre", "Nonfiction"],
+		"age":			["Age:", "3-5"],
+		"subject":		["Subject:", "Life & Relationships"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73260"]
+		
+			},
+	"book12": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Jack Canfield"],
+		"title":  		["Title:", "Chicken Soup for the Teenage Soul: 101 Stories of life, Love and Learning"],
+		"date":	  		["Date:",""],
+		"genre":		["Genre", "Nonfiction"],
+		"age":			["Age:", "Teen"],
+		"subject":		["Subject:", "Life & Relationships"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73260"]
+		
+			},
+	"book13": {
+	    "lists:": 		["Lists:", "Books read"],
+		"author": 		["Author:", "Lauren Conrad"],
+		"title":  		["Title:", "Lauren Conrad Style"],
+		"date":	  		["Date:","2012-04-21"],
+		"genre":		["Genre", "Nonfiction"],
+		"age":			["Age:", "Teen"],
+		"subject":		["Subject:", "Style & Beauty"],
+		"rate":			["Rate", "7"],
+		"comments":		["Comments", "73260"]
+		
+			},
+	"book14": {
+	    "lists:": 		["Lists:", "Books read"],
+		"author": 		["Author:", "Veronica Roth"],
+		"title":  		["Title:", "Divergent"],
+		"date":	  		["Date:","2012-05-03"],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "Teen"],
+		"subject":		["Subject:", "Science Fiction & Fantasy"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "Divergent Series #1"]
+		
+			},
+	"book15": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Christopher Paolini"],
+		"title":  		["Title:", "Inheritance"],
+		"date":	  		["Date:",""],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "Teen"],
+		"subject":		["Subject:", "Science Fiction & Fantasy"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "Inheritance Cycle Series #4"]
+		
+		},
+	"book16": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Mary Higgins Clark"],
+		"title":  		["Title:", "The Lost Years"],
+		"date":	  		["Date:",""],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Science Fiction & Fantasy"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "73261"]
+		
+		},
+	"book17": {
+	    "lists:": 		["Lists:", "Books read"],
+		"author": 		["Author:", "Walter Lsaacon"],
+		"title":  		["Title:", "Steve Jobs"],
+		"date":	  		["Date:","2012-05-03"],
+		"genre":		["Genre", "Nonfiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Biography"],
+		"rate":			["Rate", "7"],
+		"comments":		["Comments", ""]
+		
+		},
+	"book18": {
+	    "lists:": 		["Lists:", "Books read"],
+		"author": 		["Author:", "Amy Atlas"],
+		"title":  		["Title:", "Sweet Designs: Bake It, Craft It, Style It"],
+		"date":	  		["Date:","2012-05-03"],
+		"genre":		["Genre", "NonFiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Cookbooks, Food & Wine"],
+		"rate":			["Rate", "10"],
+		"comments":		["Comments", ""]
+		
+		},
+	"book19": {
+	    "lists:": 		["Lists:", "Book I will read"],
+		"author": 		["Author:", "Sissy Spacek"],
+		"title":  		["Title:", "My Extraordinary Ordinary Life"],
+		"date":	  		["Date:",""],
+		"genre":		["Genre", "Nonfiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Biography"],
+		"rate":			["Rate", "1"],
+		"comments":		["Comments", "And by Maryanne Vollers"]
+		
+				},
+	"book20": {
+	    "lists:": 		["Lists:", "Books read"],
+		"author": 		["Author:", "Nora Roberts"],
+		"title":  		["Title:", "The Last Boyfriend"],
+		"date":	  		["Date:","2012-04-21"],
+		"genre":		["Genre", "Fiction"],
+		"age":			["Age:", "Adult"],
+		"subject":		["Subject:", "Romances"],
+		"rate":			["Rate", "7"],
+		"comments":		["Comments", "Inn BoonsBoro Trilogy #2"]
 	
 	}
 }
+
